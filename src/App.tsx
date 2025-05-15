a017d0d156fd5409e88a7efc2241b436683f7398
shalomgu <guy.shalom1@gmail.com>
2025-05-15 16:37:21 +0300

    Ship

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css'
+import './GoogleSearchBar.css';
+import ShipEventsPage from './ShipEvents';
+
+
+let language = "JavaScript";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <ShipEventsPage />
+  )
+}
+
+export default App
